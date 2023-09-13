ba2fddc7f5e20536b27f74c79e88fedff5abe17b
Andres Maglione <amaglione2002@gmail.com>
2023-09-12 22:22:44 -0300

    add genetic tests

--- tp5-busqueda-local/code/resultoptions.py
+++ tp5-busqueda-local/code/resultoptions.py
@@ -41,3 +41,33 @@ def multiplot(results, queens=8):
 
 
     
+def compareGenetic(results):
+    var = results[0]
+    algorithms = results[1]
+    time = results[3]
+    solved = results[4]
+
+    fig, ax = plt.subplots(2,1, figsize=(6,12))
+    fig.suptitle(f"Comparacion de funciones de {var} para el problema de las n reinas")
+
+    #subplot 2
+    ax[0].boxplot(time, vert=True, labels=algorithms, patch_artist=True)
+    ax[0].set_ylabel('Tiempo de ejecucion (s)')
+    ax[0].yaxis.grid(True)
+
+    #subplot 3
+
+    #transformar booleanos a int suponiendo que solved es una lista de listas
+    solved = [[int(i) for i in sublist] for sublist in solved]
+
+    #calcular porcentaje
+    solved = [sum(sublist)/len(sublist) for sublist in solved]
+    
+    #plot using barchart
+    ax[1].bar(algorithms, solved)
+    ax[1].set_ylabel('Porcentaje de instancias resueltas')
+    ax[1].yaxis.grid(True)
+    ax[1].set_ylim(0,1)
+
+    plt.tight_layout()
+    plt.savefig(f'./plots/compareGenetic_{var}.png')
--- tp5-busqueda-local/code/testing.py
+++ tp5-busqueda-local/code/testing.py
@@ -1,9 +1,19 @@
 import sys
 sys.path.append("./genetic")
 
-import statistics
-import csv
+from selection import Selection
+from crossover import Crossover
+from mutation import Mutation
+from replace import Replace
+
+import statistics, csv, multiprocessing
 from genetic import Genetic
+
+
+def process_object(object):
+    return object.run()
+
+
 def test(function, params, iters):
     print("Testing: " + function.__name__)
     
@@ -45,3 +55,144 @@ def testGenetic(envs, iters):
         writer = csv.writer(f)
         writer.writerows([["Genetic", s[0].fitness, s[1], s[2], s[0].fitness == 0] for s in r])
     return heuristic, time, solved
+
+
+
+def compareGeneticSelection(envs, iters):
+    # Initialize genetic algorithms using different selection methods
+    # and compare their results
+    print('Testing genetic algorithm with different selection methods')
+    print("Proportional selection...")
+    r1 = []
+    for e in envs:
+        r1.append(Genetic(e, iters, target=0, selection=Selection(type="proportional")))
+    
+    pool = multiprocessing.Pool()
+    r1 = pool.map(process_object, r1)
+    pool.close()
+    pool.join()
+
+
+
+    print("Tournament selection...")
+    r2 = []
+    for e in envs:
+        r2.append(Genetic(e, iters, target=0, selection=Selection(type="tournament", k=8)))
+    
+    pool = multiprocessing.Pool()
+    r2 = pool.map(process_object, r2)
+    pool.close()
+    pool.join()
+    
+    print("-----------------")
+
+    heuristic1 = [s[0].fitness for s in r1]
+    time1 = [s[2] for s in r1]
+    solved1 = [s[0].fitness == 0 for s in r1]
+
+    heuristic2 = [s[0].fitness for s in r2]
+    time2 = [s[2] for s in r2]
+    solved2 = [s[0].fitness == 0 for s in r2]
+
+    print("Heuristic1: " + str(statistics.mean(heuristic1)) + " +- " + str(statistics.stdev(heuristic1)))
+    print("Time1: " + str(statistics.mean(time1)) + " +- " + str(statistics.stdev(time1)))
+    print("Solved1: " + str(sum(solved1)) + " / " + str(len(solved1)))
+    print("-----------")
+    print("Heuristic2: " + str(statistics.mean(heuristic2)) + " +- " + str(statistics.stdev(heuristic2)))
+    print("Time2: " + str(statistics.mean(time2)) + " +- " + str(statistics.stdev(time2)))
+    print("Solved2: " + str(sum(solved2)) + " / " + str(len(solved2)))
+
+    return "seleccion", ["proportional", "tournament"], [heuristic1, heuristic2], [time1, time2], [solved1, solved2]
+
+def compareGeneticMutation(envs, iters):
+    # Initialize genetic algorithms using different selection methods
+    # and compare their results
+    print('Testing genetic algorithm with different mutation methods')
+    print("Random mutation...")
+    r1 = []
+    for e in envs:
+        r1.append(Genetic(e, iters, target=0, mutation=Mutation(type="random")))
+    
+    pool = multiprocessing.Pool()
+    r1 = pool.map(process_object, r1)
+    pool.close()
+    pool.join()
+
+
+
+    print("Swap mutation...")
+    r2 = []
+    for e in envs:
+        r2.append(Genetic(e, iters, target=0, mutation=Mutation(type="swap")))
+    
+    pool = multiprocessing.Pool()
+    r2 = pool.map(process_object, r2)
+    pool.close()
+    pool.join()
+    
+    print("-----------------")
+
+    heuristic1 = [s[0].fitness for s in r1]
+    time1 = [s[2] for s in r1]
+    solved1 = [s[0].fitness == 0 for s in r1]
+
+    heuristic2 = [s[0].fitness for s in r2]
+    time2 = [s[2] for s in r2]
+    solved2 = [s[0].fitness == 0 for s in r2]
+
+    print("Heuristic1: " + str(statistics.mean(heuristic1)) + " +- " + str(statistics.stdev(heuristic1)))
+    print("Time1: " + str(statistics.mean(time1)) + " +- " + str(statistics.stdev(time1)))
+    print("Solved1: " + str(sum(solved1)) + " / " + str(len(solved1)))
+    print("-----------")
+    print("Heuristic2: " + str(statistics.mean(heuristic2)) + " +- " + str(statistics.stdev(heuristic2)))
+    print("Time2: " + str(statistics.mean(time2)) + " +- " + str(statistics.stdev(time2)))
+    print("Solved2: " + str(sum(solved2)) + " / " + str(len(solved2)))
+
+    return "mutacion", ["random", "swap"], [heuristic1, heuristic2], [time1, time2], [solved1, solved2]
+
+
+def compareGeneticReplacement(envs, iters):
+    # Initialize genetic algorithms using different selection methods
+    # and compare their results
+    print('Testing genetic algorithm with different replacement methods')
+    print("Elitist replacement...")
+    r1 = []
+    for e in envs:
+        r1.append(Genetic(e, iters, target=0, replace=Replace(type="elitism")))
+    
+    pool = multiprocessing.Pool()
+    r1 = pool.map(process_object, r1)
+    pool.close()
+    pool.join()
+
+
+
+    print("Random replacement (odds = 0.5)...")
+    r2 = []
+    for e in envs:
+        r2.append(Genetic(e, iters, target=0, replace=Replace(type="random")))
+    
+    pool = multiprocessing.Pool()
+    r2 = pool.map(process_object, r2)
+    pool.close()
+    pool.join()
+    
+    print("-----------------")
+
+    heuristic1 = [s[0].fitness for s in r1]
+    time1 = [s[2] for s in r1]
+    solved1 = [s[0].fitness == 0 for s in r1]
+
+    heuristic2 = [s[0].fitness for s in r2]
+    time2 = [s[2] for s in r2]
+    solved2 = [s[0].fitness == 0 for s in r2]
+
+    print("Heuristic1: " + str(statistics.mean(heuristic1)) + " +- " + str(statistics.stdev(heuristic1)))
+    print("Time1: " + str(statistics.mean(time1)) + " +- " + str(statistics.stdev(time1)))
+    print("Solved1: " + str(sum(solved1)) + " / " + str(len(solved1)))
+    print("-----------")
+    print("Heuristic2: " + str(statistics.mean(heuristic2)) + " +- " + str(statistics.stdev(heuristic2)))
+    print("Time2: " + str(statistics.mean(time2)) + " +- " + str(statistics.stdev(time2)))
+    print("Solved2: " + str(sum(solved2)) + " / " + str(len(solved2)))
+
+    return "reemplazo", ["elitist", "random"], [heuristic1, heuristic2], [time1, time2], [solved1, solved2]
